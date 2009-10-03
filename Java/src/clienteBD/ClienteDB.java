f1d0efe4031d76efbac0dba0bf6a78b46724d8f1
delgado.nikolas <delgado.nikolas@fec7781c-966a-11de-adcb-5b537d459081>
2009-10-03 06:49:52 +0000

    dasdassad

--- Java/src/clienteBD/ClienteDB.java
+++ Java/src/clienteBD/ClienteDB.java
@@ -13,7 +13,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import transferObjects.ClienteTO;;
+import TransferObjects.ClienteTO;;
 
 public class ClienteDB {
 
