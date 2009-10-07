92e2e23fd41613201a63a9df0f6c390f2327a96d
delgado.nikolas <delgado.nikolas@fec7781c-966a-11de-adcb-5b537d459081>
2009-10-07 01:25:54 +0000

    dsaasdasd

--- Java/src/AdministracionBD/AddRegistroBd.java
+++ Java/src/AdministracionBD/AddRegistroBd.java
@@ -1,3 +1,10 @@
+//=======================================================================
+// FECHA CREACIÓN: 30/09/09
+// AUTOR:Nicolas Delgado
+// Comentario: Esta clase guarda directamente en la base de datos todos los datos relacionado
+// con la atencion de una mascota al interior de los servicios de la clinica veterinaria.
+//======================================================================
+
 package AdministracionBD;
 
 import java.sql.Connection;
@@ -7,43 +14,51 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import TransferObjects.Usuario;
-
 import Administracion.Registro;
 
 
-
-
-public class AddRegistroBd{
+public class AddRegistroBd extends Registro{
 	Connection connection;	
 	
+	/**
+	 * Este metodo corresponde al constructor de la clase AddRegistroBd que se extiende de la super clase Registro.
+	 * @param connection Este atributo hace la conexión a la base de datos para poder guardar los datos al interior de ella.
+	 */
 	
-	public AddRegistroBd(Connection connection)
-	{		
-		this.connection = connection;	
+	public AddRegistroBd(Connection connection,int costoAtencion,String responsable,String fecha,String nombreMascota,String hora,int idServicio,String mascotaFechaNacimiento,int rutCliente) throws SQLException
+	{		 
+		super(costoAtencion,responsable,fecha,nombreMascota,hora,idServicio,mascotaFechaNacimiento,rutCliente);
+		this.connection = connection;
+				
 	}
 	
+	/**
+	 * Este metodo nos permite poder guardar los objetos al interior de la base de datos para que sean 
+	 * almacenados ahi.
+	 * 
+	 */
 
-	public String addRegistro(Registro r) throws SQLException
+	public String addRegistro() throws SQLException
 	{
-	
 		String result = null;
 		PreparedStatement insert;
 		String query="";
 		
-		query="INSERT INTO Registro (costoAtención,responsable,fecha,nombreMascota,hora,idServicio,mascotaFechaNacimiento,rutCliente) "+
+		
+		query="INSERT INTO Registro (costoAtencion,responsable,fecha,nombreMascota,hora,idServicio,mascotaFechaNacimiento,rutCliente) "+
 		      "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
 		insert = connection.prepareStatement(query);
+
+
 		
-		
-		insert.setInt(1, r.costoAtención);
-		insert.setString(2, r.responsable);
-		insert.setString(3, r.fecha);
-		insert.setString(4, r.nombreMascota);
-		insert.setString(5, r.hora);
-		insert.setInt(6, r.idServicio);
-		insert.setString(7, r.mascotaFechaNacimiento);
-		insert.setInt(8, r.rutCliente);
+		insert.setInt(1,super.costoAtencion);
+		insert.setString(2, super.responsable);
+		insert.setString(3, super.fecha);
+		insert.setString(4, super.nombreMascota);
+		insert.setString(5, super.hora);
+		insert.setInt(6, super.idServicio);
+		insert.setString(7, super.mascotaFechaNacimiento);
+		insert.setInt(8, super.rutCliente);
 		
 		
 		try 
@@ -61,35 +76,32 @@ public class AddRegistroBd{
 		
 	}
 
+	/**
+	 * Este metodo nos permite poder extraer una lista de datos del tipo Registro que se encuentran 
+	 * almacenada al interior de la base de datos.
+	 * 
+	 */
 
-	protected String deteleRegistro() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
+	@SuppressWarnings("null")
 	public List<Registro> getRegistro() throws SQLException {
 		List<Registro> register=new ArrayList<Registro>();
-    	Registro r;
     	try 
     	{
     		PreparedStatement selectAll = null;
 			ResultSet result = selectAll.executeQuery();
     		while(result.next())
     		{
-    			r= new Registro(0, null, null, null, null, 0, null, 0);
-   
-    			r.setCostoAtención(result.getInt(1));
-    			r.setResponsable(result.getString(2));
-                r.setFecha(result.getString(3));
-    			r.setNombreMascota(result.getString(4));
-    			r.setHora(result.getString(5));
-    			r.setIdServicio(result.getInt(6));
-    			r.setMascotaFechaNacimiento(result.getString(7));
-    			r.setRutCliente(result.getInt(8));
+
+    			super.setCostoAtencion(result.getInt(1));
+    			super.setResponsable(result.getString(2));
+                super.setFecha(result.getString(3));
+                super.setNombreMascota(result.getString(4));
+                super.setHora(result.getString(5));
+                super.setIdServicio(result.getInt(6));
+                super.setMascotaFechaNacimiento(result.getString(7));
+                super.setRutCliente(result.getInt(8));
     			
-    			register.add(r);
-    		}
+    			}
 		} 
     	catch (SQLException e) 
     	{
